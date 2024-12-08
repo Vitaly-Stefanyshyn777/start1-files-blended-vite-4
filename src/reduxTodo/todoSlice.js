8df828f6f2961d523e710ddd02b4395f9dfe8214
Falconoff <sokolovserega1982@gmail.com>
2024-12-08 11:57:58 +0200

    add: task2-1

--- src/reduxTodo/todoSlice.js
+++ src/reduxTodo/todoSlice.js
@@ -1,7 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-export const todoSlice = createSlice ({
-    name: 'todo',
-    initialState: {items: [], isLoading: false, error: null},
-    reducers: {}
-})
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState: { items: [], isLoading: false, error: null },
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
